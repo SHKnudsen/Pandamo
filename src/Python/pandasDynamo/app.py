9ce4c18d4f9c573ab7c2aabfdcd731c65579014c
SHKnudsen <sylvesterknudsen@gmail.com>
2019-06-20 12:11:47 +0100

    Added format dataframe endpoint to python project

--- src/Python/pandasDynamo/app.py
+++ src/Python/pandasDynamo/app.py
@@ -5,41 +5,39 @@ from flask import request
 import json
 import sys
 sys.path.append('C:/Users/SylvesterKnudsen/Documents/GitHub/Pandamo/src/Python/pandasDynamo')
-from pandas_funcs.create_dataframe.from_converted_dyn_dict import from_converted_dyn_dict
+from pandas_funcs.create_dataframe.from_converted_dyn_dict import *
+from pandas_funcs.format.tabulate_dataframe import *
  
 # initialize the flask application
 app = Flask(__name__)
  
-# endpoint api_1() with post method
-@app.route("/api/v1.0/csharp_python_restfulapi_json", methods=["POST"])
-def csharp_python_restfulapi_json():
-    """
-    simple c# test to call python restful api web service
-    """
-    try:                
-        #get request json object
+# endpoint api_1() with post method for create dataframe from dict
+@app.route("/api/v1.0/create_dataframe_from_dict", methods=["POST"])
+def create_dataframe():
+    try:
         request_json = request.get_json()
-        print(request_json)      
-        #convert to response json object 
-        response = jsonify(request_json)
-        print(response)
-        response.status_code = 200  
+        df = from_converted_dyn_dict(request_json)
+        df_json = df.to_json(orient='index')
+        response = app.response_class(
+            response=df_json,
+            status=200,
+            mimetype='application/json'
+        )
     except:
         exception_message = sys.exc_info()[1]
         response = json.dumps({"content":exception_message})
         response.status_code = 400
     return response
 
-@app.route("/api/v1.0/create_dataframe_from_dict", methods=["POST"])
-def create_dataframe():
+# endpoint api_1() with post method for create dataframe from dict
+@app.route("/api/v1.0/tabulate_dataframe", methods=["POST"])
+def format_tabulate_dataframe():
     try:
         request_json = request.get_json()
-        print(request_json)
-        df = from_converted_dyn_dict(request_json)
-        df_json = df.to_json(orient='index')
-        print("df_json" +df_json)
+        df = pd.read_json(json.dumps(request_json), orient='index')
+        df_string = tabulate_dataframe(df)
         response = app.response_class(
-            response=df_json,
+            response=df_string,
             status=200,
             mimetype='application/json'
         )
