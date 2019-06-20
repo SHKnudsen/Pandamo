990c6c002aaaf1e0731cd7b12f66584ab48b54ef
SHKnudsen <sylvesterknudsen@gmail.com>
2019-06-20 11:10:39 +0100

    Fixed showing nested json string and dictionarys

--- src/DynamoPandas/Pandas/DataFrame.cs
+++ src/DynamoPandas/Pandas/DataFrame.cs
@@ -32,10 +32,7 @@ namespace DynamoPandas.Pandas
 
         public static DataFrame ByDictionary(Dictionary dataDictionary)
         {
-            List<string> keys = dataDictionary.Keys.ToList();
-            List<object> vals = dataDictionary.Values.ToList();
-            var dict = keys.Zip(vals, (k, v) => new { k, v })
-                .ToDictionary(x => x.k, x => x.v);
+            var dict = DictionaryHelpers.ToCDictionary(dataDictionary);
             string jsonStr = JsonConvert.SerializeObject(dict, Formatting.None);
             string dataframeString = DynamoPandas.PythonRestTest
                 .CSharpPythonRestfulApiSimpleTest("http://127.0.0.1:5000/api/v1.0/create_dataframe_from_dict", jsonStr);
@@ -43,44 +40,10 @@ namespace DynamoPandas.Pandas
             return df;
         }
 
-        public static Dictionary<string, object> ToDictionary(DataFrame dataFrame)
+        public static object ToDictionary(DataFrame dataFrame)
         {
-            var obj = JObject.Parse(dataFrame.dataframeJson);
-            var dict = new Dictionary<string, object>();
-            foreach (var property in obj)
-            {
-                var name = property.Key;
-                var value = property.Value;
-
-                if (value is JArray)
-                {
-                    dict.Add(name, value.ToArray());
-                }
-                else if (value is JValue)
-                {
-                    dict.Add(name, value.ToString());
-                }
-                else if (value is JObject)
-                {
-                    foreach (var item in value)
-                    {   
-                        if (value is JArray)
-                        {
-                            dict.Add(name, value.ToArray());
-                        }
-                        else if (value is JValue)
-                        {
-                            dict.Add(name, value.ToString());
-                        }
-                    }
-                }
-                else
-                {
-                    throw new NotSupportedException("Invalid JSON token type.");
-                }
-            }
-
-            return dict;
+            object obj = JsonHelper.Deserialize(dataFrame.dataframeJson);
+            return obj;
         }
 
     }
---
+++ src/DynamoPandas/Utilities/DictionaryHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamoPandas.Utilities
+{
+    public static class DictionaryHelpers
+    {
+        public static Dictionary<string,object> ToCDictionary(DesignScript.Builtin.Dictionary dictionary)
+        {
+            List<string> keys = dictionary.Keys.ToList();
+            List<object> values = new List<object>();
+            foreach (var value in dictionary.Values)
+            {
+                if (value.GetType() == typeof(DesignScript.Builtin.Dictionary))
+                {
+                    DesignScript.Builtin.Dictionary subDict = value as DesignScript.Builtin.Dictionary;
+                    List<string> subKeys = subDict.Keys.ToList();
+                    List<object> subVals = subDict.Values.ToList();
+                    var newDict = subKeys.Zip(subVals, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+                    values.Add(newDict);
+                }
+                else
+                {
+                    values.Add(value);
+                }
+            }
+            Dictionary<string,object> cDict = keys.Zip(values, (k, v) => new { k, v })
+                .ToDictionary(x => x.k, x => x.v);
+            return cDict;
+        }
+    }
+}
