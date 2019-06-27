f6b537b132c1f61231af662d224530c54fd2d7aa
SHKnudsen <sylvesterknudsen@gmail.com>
2019-06-27 04:16:20 +0100

    added eval() to python function to turn string into dictionary

--- src/Python/pandasDynamo/pandas_funcs/create_dataframe/from_converted_dyn_dict.py
+++ src/Python/pandasDynamo/pandas_funcs/create_dataframe/from_converted_dyn_dict.py
@@ -7,7 +7,7 @@ import re
 # Create Dataframes
 def from_converted_dyn_dict(data_dict):
     try:
-        dataframe = pd.DataFrame(data_dict)
+        dataframe = pd.DataFrame(eval(data_dict))
         return dataframe
     except Exception as e:
         return e
--- src/Python/pandasDynamo/test.py
+++ src/Python/pandasDynamo/test.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from tabulate import tabulate
+import sys
+sys.path.append('C:/Users/SylvesterKnudsen/Documents/GitHub/Pandamo/src/Python/pandasDynamo')
+from utillities.string_helpers import *
 
-thisdict =	{
-  "brand": ["Ford"]
-}
-
-df = pd.DataFrame(thisdict)
-
-print(tabulate(df, headers='keys', tablefmt='orgtbl'))
+string = "[this,that]"
+test = string_to_list(string)
+print(test)
