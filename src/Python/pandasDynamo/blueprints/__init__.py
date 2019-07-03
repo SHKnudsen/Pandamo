0979df37f926bec65164ae115a82efcf23d81a3e
SHKnudsen <sylvesterknudsen@gmail.com>
2019-07-03 11:04:41 +0100

    Update __init__.py

--- src/Python/pandasDynamo/blueprints/__init__.py
+++ src/Python/pandasDynamo/blueprints/__init__.py
@@ -6,11 +6,13 @@ from blueprints.api.dataframes.create_dataframe import mod
 from blueprints.api.filters.filter_dataframe import mod
 from blueprints.api.format.format_dataframe import mod
 from blueprints.api.reshape.reshape_dataframe import mod
+from blueprints.api.select.select_rows import mod
 
 app.register_blueprint(api.dataframes.create_dataframe.mod, url_prefix='/api/create_dataframe')
 app.register_blueprint(api.filters.filter_dataframe.mod, url_prefix='/api/filter_dataframe')
 app.register_blueprint(api.format.format_dataframe.mod, url_prefix='/api/format_dataframe')
 app.register_blueprint(api.reshape.reshape_dataframe.mod, url_prefix='/api/reshape_dataframe')
+app.register_blueprint(api.select.select_rows.mod, url_prefix='/api/select_rows')
 
 from blueprints.api.error_handlers import blueprint
 
