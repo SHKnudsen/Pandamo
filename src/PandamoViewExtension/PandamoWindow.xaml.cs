b1a8960c71ed437c352aa694bc249c1d88ef4485
SHKnudsen <sylvesterknudsen@gmail.com>
2019-11-10 17:57:56 +0000

    Ui changes

--- src/PandamoViewExtension/PandamoWindow.xaml.cs
+++ src/PandamoViewExtension/PandamoWindow.xaml.cs
@@ -27,6 +27,33 @@ namespace DynamoPandas.PandamoViewExtension
         {
             pandamoVm = vm;
             InitializeComponent();
+            scrollViewer.ScrollChanged += ScrollViewer_ScrollChanged;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            bool AutoScroll = true;
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set auto-scroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset auto-scroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and auto-scroll mode set
+                // Autoscroll
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.ExtentHeight);
+            }
         }
 
         private void StartServerButton_Click(object sender, RoutedEventArgs e)
